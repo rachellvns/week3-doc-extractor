2a32a23683b9013f6e61028c2fe5aa2dbbf0e66c
rachellvns <rachellvannessachr@gmail.com>
2026-08-01 13:38:23 +0800

    Add Pydantic schemas for telemedicine consultation extraction

---
+++ schema.py
@@ -0,0 +1,43 @@
+# The purpose of this file is to validate the output schema from the LLM
+
+from pydantic import BaseModel, Field
+from typing import Literal
+
+class Patient(BaseModel):
+    name: str = Field(min_length=1)
+    age: float | None = Field(default=None,ge=0) # to accomodate baby <1 year
+    sex: Literal["Male", "Female"] | None = None
+    
+class Diagnosis(BaseModel):
+    condition: str
+    certainty: str | None = None # example: likely, possible, confirmed
+    status: str | None = None # example: improving, stable, controlled, etc
+
+class Medication(BaseModel):
+    medicine_name: str
+    dosage: float | None = Field(default=None, gt=0)
+    unit: str | None = None
+    frequency: str | None = None
+    duration: str | None = None
+    additional_notes: str | None = None # example: before/after meal
+    
+class FollowUp(BaseModel):
+    timeframe: str | None = None
+    instructions: str | None = None
+    
+class Consultation(BaseModel):
+    consultation_id: str = Field(pattern=r"^(TM-\d{4}-\d{4}|\d{4}-\d{4})$")
+    date: str | None = None
+    patient: Patient
+    history: str | None = None
+    reason: str | None = None
+    diagnoses: list[Diagnosis] = Field(default_factory=list) # to avoid mutability
+    medications: list[Medication] = Field(default_factory=list) # to avoid mutability
+    action_plan: list[str] = Field(default_factory=list) # to avoid mutability
+    provider: str | None = None
+    clinical_notes: str | None = None
+    follow_up: FollowUp | None = None
+    
+    
+    
+    
