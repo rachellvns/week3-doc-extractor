fbf4779b30057f83daf1722c8a8888bcb0412426
rachellvns <rachellvannessachr@gmail.com>
2026-08-01 17:59:59 +0800

    Revise age constraint

--- schema.py
+++ schema.py
@@ -5,14 +5,14 @@ from typing import Literal
 
 class Patient(BaseModel):
     name: str = Field(min_length=1)
-    age_value: float | None = None
+    age_value: float | None = Field(default=None, ge=0)
     age_unit: Literal["year(s)", "month(s)", "week(s)", "day(s)"] | None = None
     sex: Literal["Male", "Female"] | None = None
     
     @model_validator(mode="after")
     def validate_age_format(self):
         if self.age_value is not None and self.age_unit is not None:
-            if self.age_unit == "years" and self.age_value % 1 != 0:
+            if self.age_unit == "year(s)" and self.age_value % 1 != 0:
                 raise ValueError(
                     "Age in years must be a whole number"
                 )
@@ -36,7 +36,7 @@ class FollowUp(BaseModel):
     instructions: str | None = None
     
 class Consultation(BaseModel):
-    consultation_id: str = Field(pattern=r"^(TM-\d{4}-\d{4})$", default = None)
+    consultation_id: str | None = Field(default=None,pattern=r"^TM-(\d{4}|null)-(\d{4}|null)$")
     date: str | None = None
     patient: Patient
     patient_history: str | None = None
